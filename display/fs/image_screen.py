a4269268d468b727f530df6451c8a56d51dfd1dc
Dmytro Panin <panin.dimitriy@gmail.com>
2021-01-14 22:58:38 +0200

    Created an eink style screen that saves it's output to an image on the fs.

--- display/fs/image_screen.py
+++ display/fs/image_screen.py
@@ -1,36 +1,24 @@
 import os
 
 from PIL import Image, ImageDraw, ImageFont
-from waveshare_epd import epd2in13_V2
 
 from data.plot import Plot
 from display.observer import Observer
 
-SCREEN_HEIGHT = epd2in13_V2.EPD_WIDTH  # 122
-SCREEN_WIDTH = epd2in13_V2.EPD_HEIGHT  # 250
+SCREEN_HEIGHT = 122
+SCREEN_WIDTH = 250
 
 FONT_SMALL = ImageFont.truetype(os.path.join(os.path.dirname(__file__), '../Font.ttc'), 13)
 FONT_LARGE = ImageFont.truetype(os.path.join(os.path.dirname(__file__), '../Font.ttc'), 24)
 
 
-class Epd2in13V2(Observer):
+class ImageScreen(Observer):
 
-    def __init__(self, observable):
+    def __init__(self, observable, filename):
         super().__init__(observable=observable)
-        self.epd = epd2in13_V2.EPD()
-        self.screen_image = self._init_display(self.epd)
-
-    @staticmethod
-    def _init_display(epd):
-        epd.init(epd.FULL_UPDATE)
-        epd.Clear(0xFF)
-        screen_image = Image.new('1', (SCREEN_WIDTH, SCREEN_HEIGHT), 255)
-        epd.displayPartBaseImage(epd.getbuffer(screen_image))
-        epd.init(epd.PART_UPDATE)
-        return screen_image
+        self.filename = filename
 
     def form_image(self, prices, screen_draw):
-        screen_draw.rectangle((0, 0, SCREEN_WIDTH, SCREEN_HEIGHT), fill="#ffffff")
         max_price = max(prices)
         min_price = min(prices)
         middle_price = (max_price - min_price) / 2 + min_price
@@ -48,12 +36,8 @@ class Epd2in13V2(Observer):
         screen_draw.text(price_position, price_text, font=FONT_LARGE)
 
     def update(self, data):
-        self.form_image(data, ImageDraw.Draw(self.screen_image))
-        screen_image_rotated = self.screen_image.rotate(180)
-        # TODO: add a way to switch bewen partial and full update
-        # epd.display(epd.getbuffer(screen_image_rotated))
-        self.epd.displayPartial(self.epd.getbuffer(screen_image_rotated))
+        image = Image.new('1', (SCREEN_WIDTH, SCREEN_HEIGHT), 255)
+        self.form_image(data, ImageDraw.Draw(image))
+        image.save(self.filename)
 
-    @staticmethod
-    def close():
-        epd2in13_V2.epdconfig.module_exit()
+
