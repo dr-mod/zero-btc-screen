c493e21cb2593bf333a2dc53cc3c792ab1c6cb7a
Dmytro Panin <drmod@Dmytros-MacBook-Pro.local>
2025-01-30 23:58:57 +0100

    Superficial clean up of the main file.

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 import json
-import random
 import time
 import requests
 import urllib.parse
 from datetime import datetime, timezone, timedelta
 from urllib.error import HTTPError, URLError
-from urllib.request import Request, urlopen
 
 from config.builder import Builder
 from config.config import config
@@ -17,7 +15,8 @@ DATETIME_FORMAT = "%Y-%m-%dT%H:%M"
 
 
 def get_dummy_data():
-    logger.info('Generating dummy data')
+    # TODO: Implement functionality to provide dummy data for testing purposes.
+    return []
 
 
 
@@ -26,7 +25,8 @@ def fetch_prices():
     timeslot_end = datetime.now(timezone.utc)
     end_date = timeslot_end.strftime(DATETIME_FORMAT)
     start_data = (timeslot_end - timedelta(days=DATA_SLICE_DAYS)).strftime(DATETIME_FORMAT)
-    url = f'https://api.exchange.coinbase.com/products/{config.currency}/candles?granularity=900&start={urllib.parse.quote_plus(start_data)}&end={urllib.parse.quote_plus(end_date)}'
+    url = (f'https://api.exchange.coinbase.com/products/{config.currency}/candles?'
+           f'granularity=900&start={urllib.parse.quote_plus(start_data)}&end={urllib.parse.quote_plus(end_date)}')
     headers = {"Accept": "application/json"}
     response = requests.request("GET", url, headers=headers)
     external_data = json.loads(response.text)
